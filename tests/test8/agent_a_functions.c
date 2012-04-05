76988cfb66fd883143af5ef6572dd34a1ed3af8a
Simon Coakley <sysfal@gmail.com>
2012-04-05 09:56:25 +0000

    Add test 8 to look at the use of not tags in conditions

---
+++ tests/test8/agent_a_functions.c
@@ -0,0 +1,20 @@
+#include "header.h"
+#include "agent_a_agent_header.h"
+
+/** \fn agent_a_1()
+ * \brief A description of the function
+ */
+int agent_a_1()
+{
+
+	return 0;
+}
+
+/** \fn agent_a_2()
+ * \brief A description of the function
+ */
+int agent_a_2()
+{
+	
+   return 0;
+}
